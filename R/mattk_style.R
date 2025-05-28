4d31cfc6e677f16acdf2b6093e2b6c19815d97d7
Matthew Knowles <mattknowles314@proton.me>
2025-05-28 21:10:27 +0100

    fix style

--- R/mattk_style.R
+++ R/mattk_style.R
@@ -2,7 +2,7 @@
 #' 
 #' @export 
 mattk_style <- function() {
-    ggplot2::theme_set(theme_mattk)
+    ggplot2::theme_set(theme_mattk())
     options(
         ggplot2.discrete.fill = mattk_colours,
         ggplot2.discrete.colour = mattk_colours
